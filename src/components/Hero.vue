71370a916b2bcd1bd7c52c6bef5fd3dfe6055116
Cheosphere <ebae82@hotmail.com>
2023-11-14 16:51:38 -0300

    Cargando proyecto en repositorio remoto_v3

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -5,20 +5,20 @@ const heroContent = [
     {
         title: 'Discover innovative ways to decorate',
         content: 'We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.',
-        desktop_img: 'src/assets/images/desktop-image-hero-1.jpg',
-        mobile_img: 'src/assets/images/mobile-image-hero-1.jpg'
+        desktop_img: '/room-homepage-master/assets/desktop-image-hero-1.jpg',
+        mobile_img: '/room-homepage-master/assets/mobile-image-hero-1.jpg'
     },
     {
         title: 'We are available all across the globe',
         content: "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.",
-        desktop_img: 'src/assets/images/desktop-image-hero-2.jpg',
-        mobile_img: 'src/assets/images/mobile-image-hero-2.jpg'
+        desktop_img: '/room-homepage-master/assets/desktop-image-hero-2.jpg',
+        mobile_img: '/room-homepage-master/assets/mobile-image-hero-2.jpg'
     },
     {
         title: 'Manufactured with the best materials',
         content: 'Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.',
-        desktop_img: 'src/assets/images/desktop-image-hero-3.jpg',
-        mobile_img: 'src/assets/images/mobile-image-hero-3.jpg'
+        desktop_img: '/room-homepage-master/assets/desktop-image-hero-3.jpg',
+        mobile_img: '/room-homepage-master/assets/mobile-image-hero-3.jpg'
     }
 ]
 
